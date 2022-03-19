608e5416e949695a47e299669f6b7027c8ea699e
LinearJester <linearjester@gmail.com>
2022-03-19 23:56:27 +0100

    add example unit test

---
+++ UTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Hello test!!!");
+        }
+    }
+}
