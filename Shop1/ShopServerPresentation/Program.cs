2407a0f76d0bcb78491867f444c89efe41d2c213
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2022-05-15 02:59:03 +0200

    Basic websocket server

--- Shop1/ShopServerPresentation/Program.cs
+++ Shop1/ShopServerPresentation/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
 
 namespace ShopServerPresentation
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Server started");
+            await WebSocketServer.Server(8081, ConnectionHandler);
+        }
+
+        static void ConnectionHandler(WebSocketConnection webSocketConnection)
+        {
+            
+            webSocketConnection.onMessage = ParseMessage;
+            webSocketConnection.onClose = () => { Console.WriteLine("[Server]: Connection closed"); };
+            webSocketConnection.onError = () => { Console.WriteLine("[Server]: Connection error encountered"); };
+        }
+
+        static async void ParseMessage(string message)
+        {
+            Console.WriteLine($"[Client]: {message}");
         }
     }
 }
