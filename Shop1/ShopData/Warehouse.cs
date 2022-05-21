1e70e06da4ba17623995ff1758474a4f8c96c924
LinearJester <linearjester@gmail.com>
2022-05-21 23:04:30 +0200

    initialize list

--- Shop1/ShopData/Warehouse.cs
+++ Shop1/ShopData/Warehouse.cs
@@ -11,6 +11,7 @@ namespace ShopData
         public Warehouse()
         {
             Stock = new List<IFruit>();
+            observers = new List<IObserver<IFruit>>();
 
             waitingForStockUpdate = false;
 
